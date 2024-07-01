1bdac19f20385153a0b22d9014e5a9fba0a526be
OmegaGiven <omegagiven9@gmail.com>
2024-07-01 17:30:08 -0500

    finished zoom system for zooming in and out, could use refinement later when adding a settings menu

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,6 +6,9 @@ var deadzone = 0.1
 const SPEED = 300.0
 var keyboard = false
 var free_cam = false
+var MAX_ZOOM = Vector2(5,5)
+var MIN_ZOOM = Vector2(0.25, 0.25)
+var ZOOM_SPEED = 0.1
 
 func movement():
 	# produces velocity by returning Vector2D
@@ -22,17 +25,16 @@ func movement():
 											"ui_up{n}".format({"n":device_num}),
 											"ui_down{n}".format({"n":device_num})
 											)
-	velocity = input_direction * SPEED
-	return velocity
+	var v = input_direction * SPEED
+	return v
 
 
 func move_camera():
-	#TODO
+	SplitScreenFunctionality.player_characters[device_num]["camera"].position += movement() * .1
 	# get camera for player will setup global for this
 	# turn vector movement to translate camera
 	# include code to manipulate camera scale with right stick
 	# include rotation if we deem necessary
-	pass
 
 func direction_manager():
 	#TODO get animation direction and apply to player sprite
@@ -42,19 +44,36 @@ func direction_manager():
 
 func _physics_process(_delta):
 	if not free_cam:
-		movement()
+		velocity = movement()
 	move_and_slide()
 
 func _input(event):
 	#this will error until I find key for it but on keyboard as its for controller currently
-	if event.is_action_released("D_DOWN_action{n}".format({"n":device_num})) :
+	if event.is_action_released("D_DOWN_action{n}".format({"n":device_num})):
 		# toggle free camera mode
 		free_cam = !free_cam
-		print("Player {n} entered freecam".format({"n": device_num}))
+		if free_cam:
+			print("Player {n} entered freecam".format({"n": device_num}))
+		else:
+			print("Player {n} exited freecam".format({"n": device_num}))
+	
+	## zoom Camera (this is glitchy as its only upon movement of joystick but not holding the joystick in position)
+	#if event.is_action("ui_upR{n}".format({"n":device_num})) or event.is_action("ui_downR{n}".format({"n":device_num})):
+		#var zoom = SplitScreenFunctionality.player_characters[device_num]["camera"].zoom - (Vector2(event.axis_value, event.axis_value) * 0.1)
+		#if zoom < MAX_ZOOM and zoom > MIN_ZOOM:
+			#SplitScreenFunctionality.player_characters[device_num]["camera"].zoom = zoom
 
 
 func _process(_delta):
-	
-	#if camera mode
-	# move camera()
-	pass
+	if free_cam:
+		move_camera()
+	else:
+		SplitScreenFunctionality.player_characters[device_num]["camera"].position = self.position
+
+	# zoom Camera "smoothish"
+	if Input.get_joy_axis(device_num, JOY_AXIS_RIGHT_Y) > deadzone or Input.get_joy_axis(device_num, JOY_AXIS_RIGHT_Y) < -deadzone:
+		var zoom = Vector2(Input.get_joy_axis(device_num, JOY_AXIS_RIGHT_Y), Input.get_joy_axis(device_num, JOY_AXIS_RIGHT_Y))
+		var zoom_check = SplitScreenFunctionality.player_characters[device_num]["camera"].zoom + (ZOOM_SPEED * zoom)
+		if zoom_check < MAX_ZOOM and zoom_check > MIN_ZOOM:
+			SplitScreenFunctionality.player_characters[device_num]["camera"].zoom = zoom_check
+
