10142ae926ca177729cbd54106b37edff190d354
OmegaGiven <omegagiven9@gmail.com>
2024-07-12 08:32:11 -0500

    cleaning old test files that are not needed

--- scripts/ExperimentalOmega.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready():
-	self.add_child(SplitScreenFunctionality.scene_splitscreen)
-	SplitScreenFunctionality.add_splitscreen_to_scene()
--- scripts/global scripts/GamePaths.gd
+++ scripts/global scripts/GamePaths.gd
@@ -2,13 +2,14 @@ extends Node
 
 
 #LEVELS
-@export var arcade_match = "res://scenes/Arcade Level/Arcade Match.tscn"
+@export var arcade_match 	= "res://scenes/Arcade Level/Arcade Match.tscn"
+@export var tutorial_level	= "res://scenes/Tutorial/Tutorial.tscn"
 
 
 #OBJECTS
-@export var Player = "res://scenes/Player.tscn"
-@export var Tower = "res://scenes/Arcade Level/Arcade Match.tscn"
+@export var Player 			= "res://scenes/Player.tscn"
+@export var Tower			= "res://scenes/Objects/Tower.tscn"
 
 
 #UI
-@export var GamePaths = "res://scenes/BuildMenu.tscn"
+@export var BuildMenu 		= "res://scenes/BuildMenu.tscn"
