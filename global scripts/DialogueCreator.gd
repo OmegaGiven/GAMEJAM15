3020aee7809ec725f8505390953fd6eb76fbcbba
OmegaGiven <omegagiven9@gmail.com>
2024-07-18 16:32:30 -0500

    Making a outline for Tutorial level. also added some tilemaps that could be used to theme the tutorial

--- global scripts/DialogueCreator.gd
+++ global scripts/DialogueCreator.gd
@@ -1,6 +1,5 @@
 extends Node
 
-
 var dialogue_opened = false
 var skip_dialogue_typing = false
 var TEXT_SPEED = 0.2
@@ -13,8 +12,8 @@ func TypeText(text, label):
 
 
 func CreateDialogue(text_chunk):
-	var dialogue_box = CanvasLayer.new()
-	var dialogue_label = RichTextLabel.new()
+	var dialogue_box = CanvasLayer.new().instantiate()
+	var dialogue_label = RichTextLabel.new().instantiate()
 	dialogue_label.anchors_preset = 5 #5: Center Top
 	dialogue_label.size.x = Settings.resolution_x * 0.9
 	dialogue_label.size.y = Settings.resolution_y * 0.2
