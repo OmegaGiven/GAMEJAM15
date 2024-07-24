d3d570b04d98b2c18347060ae3a99091025d3b52
solek7 <soleiker787@gmail.com>
2024-07-24 23:48:02 +0300

    Added Hp Bar script, changed a bit the animation speed of fire on the bonefire and you can press K to delete 1 hp from the bonefire - also the bonefire hp can be set inside its scene, in the hp bar node or smthing

--- scenes/HpBar.gd
+++ scenes/HpBar.gd
@@ -1,11 +1,45 @@
 extends Control
 
-const HP_TICK = preload("res://scenes/hp_tick.tscn")
+
+@onready var progress_bar = $progressBar
+@onready var hp_Container = $HpContainer
+ 
+@export var sprite : Sprite2D
+@export var health_texture : PackedScene
+
+@export var max_health = 5
+
+var cur_health : int = 0
+var max_health_size
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
-
+	reciveHp(max_health)
+	max_health_size = 3 + 4 * max_health
+	cur_health = max_health
+	progress_bar.size.x = (max_health_size)
+	progress_bar.position.x = sprite.position.x
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func reciveHp(health_recived):
+	
+	if cur_health >= max_health:
+		return
+	for i in health_recived:
+		var hp_tick = health_texture.instantiate()
+		hp_Container.add_child(hp_tick)
+		cur_health += 1
+		
+
+func damage(damage_recived):
+	for i in damage_recived:
+		hp_Container.get_child(hp_Container.get_child_count()-1).queue_free()
+		cur_health -= 1 
+		if cur_health == 0:
+			get_parent().queue_free()
+			break
+	
--- scenes/Objects/bonfire.gd
+++ scenes/Objects/bonfire.gd
@@ -1,5 +1,10 @@
 extends RigidBody2D
 
+@export var hp_bar : Control
 @export var health = 100
 @export var type = "base"
+
+func _process(delta):
+	if Input.is_action_just_pressed("debug"):
+		hp_bar.damage(1)
 
