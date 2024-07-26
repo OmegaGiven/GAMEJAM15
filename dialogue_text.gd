6ec0f03f8501bdc1e9d1f48e8de0bbdafb317e34
OmegaGiven <omegagiven9@gmail.com>
2024-07-26 09:37:57 -0500

    added footsteps and background audio, hid arcade game as its not built

--- dialogue_text.gd
+++ dialogue_text.gd
@@ -2,7 +2,10 @@ extends Node
 
 
 var tutorial_text = {
-	"welcome" : ["First", "Second", "Third"]
+	"Intro" : ["Where am I?", "How did I get here?", "Is that a camp fire up ahead?"],
+	"ArriveAtFire" : ["Looks like it could use some wood", "lets go hack away some trees"],
+	"EnemyComing" : ["What is that sound?", "Those dont look friendly"],
+	"FirstEncounterFinished": ["Where did those come from?", "I should build some torches in case more come"],
 	
 	
 }
